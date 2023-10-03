73194a4a52a727f25bc2a910baf761bc7b5b9252
rajmoham <rajmoham10@gmail.com>
2023-10-03 23:11:24 +0100

    tests passed

--- primes.py
+++ primes.py
@@ -1,6 +1,30 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+    else:
+        count = 0
+        num = 2
+        while count < number_of_primes:
+            if (isPrime(num)):
+                list.append(num)
+                count += 1
+            num += 1
+
     return list
+
+
+def isPrime(num):
+    upper_check = math.floor(math.sqrt(num))
+    for i in range(2, upper_check + 1):
+        if (num % i == 0):
+            return False
+    return True
+
+    
